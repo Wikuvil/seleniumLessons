0edbb541fa243619203897c50ac8e4d12bb9d11e
Wikuvil <rksimikhail@gmail.com>
2022-04-28 19:27:12 +0300

    Fix warning

---
+++ lessons/Lesson_11.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+link = 'http://suninjuly.github.io/alert_accept.html'
+browser = webdriver.Chrome()
+try:
+    browser.get(link)
+    browser.find_element(By.CLASS_NAME, 'btn-primary').click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = x_element.text
+    func = calc(x)
+    input_answer = browser.find_element(By.ID, 'answer')
+    input_answer.send_keys(func)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+finally:
+    print(browser.switch_to.alert.text.split(': ')[-1])
+    browser.quit()
+
